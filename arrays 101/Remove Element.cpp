566115186e5f37e7d0bfd36c34cdd999ea075b15
Memeladon <bingorussian@mail.ru>
2023-02-09 15:03:55 +0300

    deleting-items-from-an-array

---
+++ arrays 101/Remove Element.cpp
@@ -0,0 +1,14 @@
+/*
+Given an integer array nums and an integer val, remove all occurrences of val in nums in-place. The relative order of the elements may be changed.
+Since it is impossible to change the length of the array in some languages, you must instead have the result be placed in the first part of the array nums. More formally, if there are k elements after removing the duplicates, then the first k elements of nums should hold the final result. It does not matter what you leave beyond the first k elements.
+Return k after placing the final result in the first k slots of nums.
+*/
+//https://leetcode.com/explore/learn/card/fun-with-arrays/526/deleting-items-from-an-array/3247/
+
+class Solution {
+public:
+    int removeElement(std::vector<int>& nums, int val) {
+        nums.erase(std::remove(nums.begin(), nums.end(), val), nums.end());
+        return int(nums.size());
+    }
+};
